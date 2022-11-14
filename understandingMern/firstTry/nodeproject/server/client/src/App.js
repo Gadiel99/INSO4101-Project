4e2647a34b0be9ea84fc4cf14ad5521024fa1ae6
SebastianSaliva <sebastian.saliva@upr.edu>
2022-11-14 18:08:12 -0400

    committtttttttttt

---
+++ understandingMern/firstTry/nodeproject/server/client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        BACKGROUNDMEETS
+        <a href="http://localhost:5000">Sign In</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
